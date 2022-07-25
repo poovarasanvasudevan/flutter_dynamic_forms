cbf97a2bada3e53bde29f87182d71780c01bd9f7
Poovarasan Vasudevan <poosan9@gmail.com>
2022-07-25 12:20:52 +0530

    padding removed

--- packages/flutter_dynamic_forms_components/lib/src/components/radio_button_group/default_radio_button_group_renderer.dart
+++ packages/flutter_dynamic_forms_components/lib/src/components/radio_button_group/default_radio_button_group_renderer.dart
@@ -12,9 +12,6 @@ class DefaultRadioButtonGroupRenderer
       FormElementRendererFunction renderer) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-        ),
         ...element.choices
             .where((c) => c.isVisible)
             .map((choice) => renderer(choice, context))
--- packages/flutter_dynamic_forms_components/lib/src/components/radio_button_group/radio_button_group_renderer.dart
+++ packages/flutter_dynamic_forms_components/lib/src/components/radio_button_group/radio_button_group_renderer.dart
@@ -20,9 +20,6 @@ class RadioButtonGroupRenderer extends FormElementRenderer<RadioButtonGroup> {
       builder: (context, _) {
         return Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-            ),
             ...element.choices
                 .where((c) => c.isVisible)
                 .map((choice) => renderer(choice, context))
